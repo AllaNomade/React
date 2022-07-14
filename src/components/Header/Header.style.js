bb937072c34ef5615b65dc31b59eefe65e5c97b8
Allan <allanpaiva17@gmail.com>
2022-07-14 13:51:51 -0300

    added ESLint fixes

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,8 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.header`
-${({ theme }) => css`
- background-color: ${theme.colors.gray.dark}
-`}
-
+  ${({ theme }) => css`
+    background-color: ${theme.colors.gray.dark}
+  `}
 `;
